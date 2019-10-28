3ff630bdad54bed0238d83bcf3b6b008cfd6d528
Evgeniy Shtro <augustalonso@gmail.com>
2019-10-28 20:07:19 +0200

    Init Colshape class declaration

--- class/colshape.d.ts
+++ class/colshape.d.ts
@@ -1,3 +1,15 @@
 declare class ColShape extends Element {
+  readonly shapeType: number;
 
+  // lua_classfunction(luaVM, "Circle", "createColCircle");
+  // lua_classfunction(luaVM, "Cuboid", "createColCuboid");
+  // lua_classfunction(luaVM, "Rectangle", "createColRectangle");
+  // lua_classfunction(luaVM, "Sphere", "createColSphere");
+  // lua_classfunction(luaVM, "Tube", "createColTube");
+  // lua_classfunction(luaVM, "Polygon", "createColPolygon");
+
+  // lua_classfunction(luaVM, "getElementsWithin", "getElementsWithinColShape");
+  // lua_classfunction(luaVM, "isInside", "isInsideColShape");
+
+  // lua_classfunction(luaVM, "getShapeType", "getColShapeType");
 }
