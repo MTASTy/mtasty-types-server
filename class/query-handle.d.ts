1195414100ea1943da40b8fc22005de329abd3bd
Evgeniy Shtro <augustalonso@gmail.com>
2019-10-29 21:35:24 +0200

    Add QueryHandle class declaration

--- class/query-handle.d.ts
+++ class/query-handle.d.ts
@@ -1,3 +1,18 @@
 declare class QueryHandle {
+  /**
+   * This function checks the progress of a database query.
+   * @param timeout How many milliseconds to wait for a result. Use 0 for an instant response (which may return nil). Use -1 to wait until a result is ready. Note: A wait here will freeze the entire server just like the executeSQL* functions.
+   * @param [multipleResults=false] Set to true to enable the return values from multiple queries.
+   * @returns Result, false or undefined.
+   * @see https://wiki.mtasa.com/wiki/DbPoll
+   **/
+  poll(timeout: number, multipleResults?: boolean): DbQueryResult | undefined | false;
 
+  /**
+   * This function frees a database query handle.
+   * dbFree only needs to be used if a result has not been obtained with dbPoll.
+   * @returns Returns true if the handle was successfully freed, false otherwise.
+   * @see https://wiki.mtasa.com/wiki/DbFree
+   **/
+  free(): boolean;
 }
